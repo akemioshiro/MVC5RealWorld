2d2c2d26ea98e67cd37ccd77dc98547ef566f37d
akemioshiro@hotmail.com <akemioshiro@hotmail.com>
2017-04-08 21:11:27 -0300

    ASP.NET MVC 5 com EDMX

---
+++ MVC5RealWorld/Models/ViewModel/UserLoginView.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC5RealWorld.Models.ViewModel
+{
+    public class UserLoginView
+    {
+        [Key]
+        public int SYSUserID { get; set; }
+        [Required(ErrorMessage ="*")]
+        [Display(Name ="Login ID")]
+        public string LoginName { get; set; }
+        [Required(ErrorMessage ="*")]
+        [Display(Name ="Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+    }
+}
